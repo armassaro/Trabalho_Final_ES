816d23bbc91057eb594e33335cc628be322985d6
Michel Rochytor <michelrochytor.of@gmail.com>
2025-06-27 15:26:13 -0300

    subindo o codigo da ia

---
+++ ia.py
@@ -0,0 +1,16 @@
+import ollama
+
+def ask_deepseek(question, model="deepseek-r1:1.5B"):
+    response = ollama.generate(
+        model=model,
+        prompt=f"Responda de forma concisa e técnica e em português brasil por favor: {question}",
+        options={'temperature': 0.1} 
+    )
+    return response['response']
+
+pergunta = "Explique teoria da computação e suas aplicações práticas."
+resposta = ask_deepseek(pergunta)
+
+print("Pergunta:\n", pergunta)
+print("\nResposta do DeepSeek:\n", resposta)
+
