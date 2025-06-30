2a8e7d276d81f43c98a043af6f6b944db8640c0b
Michel Rochytor <michelrochytor.of@gmail.com>
2025-06-30 13:44:10 -0300

    controller ia

--- controller/controller_ia.py
+++ controller/controller_ia.py
@@ -2,9 +2,6 @@ from models.model_ia import IAModel
 from view import interface_principal
 
 class IAController:
-    """
-    Controller: Orquestra o fluxo de dados entre o Model e a View.
-    """
     def __init__(self, model: IAModel, view: interface_principal):
         self.model = model
         self.view = view
